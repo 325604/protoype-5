3c15dc364941c0fbdf4dbd4fc3fc206f86501d56
Rick Letschert <625604@student.mboutrecht.nl>
2020-01-28 11:39:40 +0100

    no message

---
+++ Assets/scripts/taget.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class taget : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private GameManager gameManager;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -2;
+
+    public ParticleSystem explosionparticle;
+    public int pointValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game manager").GetComponent<GameManager>();
+        
+        targetRb.AddForce(Randomforce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionparticle, transform.position, explosionparticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver(); 
+        }
+       
+    }
+
+    Vector3 Randomforce()
+    {
+        return Vector3.up * Random.Range(minSpeed,maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+}
